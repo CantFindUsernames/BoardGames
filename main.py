3e0b3500d623ce7a4ea35db2512247fe9e1c001c
Neo <joseschu@woodland.on.ca>
2023-01-10 07:31:03 -0500

    January 10, 2022 For varification of counting through the settlement list in board function

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ hexes = []
 settle = ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*",
           "*", "*", "*", "*", "*", "*", "*", "*", ]
 road = ["-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-",
-        "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", ]
+        "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
+
 for i in range(0, 20):
     res = random.choice(resources)
     num = random.choice(numbers)
@@ -19,30 +20,91 @@ for i in range(0, 20):
     hexes.append(square)
 
 
+def blank():
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+
+
 def board():
+    print("1           2           3           4           5")
     for i in range(0, 5):
         if i == 4:
-            print(settle[i])
-            print(road[5], end=" ")
+            print(settle[i] + "   1")
+            print(road[i], end=" ")
             break
-        print(settle[i], "    ", road[i], end="    ") # variables are correct up to here. Must correct from here on
+        print(settle[i], "    ", road[i], end="    ")  # variables are correct up to here. Must correct from here on
     for c in range(0, 20):
         if c % 4 == 0 and c != 0:
+            a = c // 4
             print()
             for i in range(0, 5):
                 if i == 4:
-                    print(settle[c])
-                    print(road[c], "", end="")
+                    print(settle[(5 * a) + i], " ", 1 + (c // 4))
+                    print(road[(5 * a) + i], "", end="")
                     break
-                print(settle[5 + c], "    ", road[5 + c], end="    ")
-        print(hexes[c], " ", end=road[c] + " ")
+                print(settle[(5 * a) + i], "    ", road[(5 * a) + i], end="    ")
+        print(hexes[c], " ", end=road[c + 5] + " ")
     print()
     for i in range(0, 5):
         if i == 4:
-            print(settle[i])
+            print(settle[i + 25], "  6")
             break
-        print(settle[i], "    ", road[i], end="    ")
+        print(settle[i + 25], "    ", road[i], end="    ")
 
 
+# Now the game can start
+print("Welcome to Settlers of Catan!")
 board()
-#  Road text once built ━, maybe not, actually. Maybe they input what they want their road to look like
+P1 = input("Player one, please input you first initial. It will be used to mark settlements, uppercase will be city:")
+P1r = input("Player one, please input a symbol you would like to use for roads, it cannot be \'-\':")
+P2 = input("Player two, please input you first initial. It will be used to mark settlements, uppercase will be city:")
+P2r = input("Player two, please input a symbol you would like to use for roads, it cannot be \'-\':")
+P3 = input("Player three, please input you first initial. It will be used to mark settlements, uppercase will be city:")
+P3r = input("Player three, please input a symbol you would like to use for roads, it cannot be \'-\':")
+users = [P1, P2, P3]
+roads = [P1r, P2r, P3r]
+x = 0
+for i in users:
+    blank()
+    set = input(str(i) + " Where would you like to put your first settlement? (xy)")
+    x = int(set[0])
+    y = (int(set[1])) * 5
+    set = (y - (5 - x)) - 1
+    settle[set] = i.lower()
+    board()
+    rode = input(str(i) + " Where would you like to put your first road? Please note for roads the value of each coordinate doubles and rounds down to the nearest odd number and the corresponding coordinate is added inbetween.(xy)")
+    x = int(rode[0])
+    y = (int(rode[1])) * 5
+    rode = (y - (5 - x)) - 1
+    road[rode] = roads[x].lower()
+    board()
+    x += 1
+    print(road)
